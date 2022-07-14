15dd2605b1479a40288d6042d33a1a5cec462864
Jeffrey Martinez <martinezjf2@gmail.com>
2022-07-14 18:01:49 -0400

    starter code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function() {
+console.log([drivers[0] + drivers[1]])
+
+};
+
+
+
+
+const returnLastTwoDrivers = function (drivers) {
+   console.log([drivers[2], drivers[3]])
+
+};
+
+function selectingDrivers() {
+  return [returnFirstTwoDrivers, returnLastTwoDrivers];
+}
+
+const createFareMultiplier = function (multiplyValue, value) {
+  return multiplyValue * value;
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
+};
+//   Link to the repo: https://github.com/martinezjf2/phase-1-first-class-functions-lab/blob/fis-wip/index.js
