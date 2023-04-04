6111342a2fd54e824a93e4a0eebdc209ff9ce225
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-04 12:58:23 -0400

    mock session prep

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
+
+const returnFirstTwoDrivers = function () {
+  console.log([drivers[0] + drivers[1]]);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  console.log([drivers[2], drivers[3]]);
+};
+
+function selectingDrivers() {
+  return [returnFirstTwoDrivers, returnLastTwoDrivers];
+}
+
+const createFareMultiplier = function (multiplyValue, value) {
+  return multiplyValue * value;
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
+};
